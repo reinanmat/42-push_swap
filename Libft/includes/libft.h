900382f4c0e6fe9671ad0ef9572626c9dac2e702
reinanmat <reinan266@gmail.com>
2023-01-19 19:19:18 -0300

    Add Libft

---
+++ Libft/includes/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 20:11:21 by revieira          #+#    #+#             */
+/*   Updated: 2023/01/02 11:05:17 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "macros.h"
+# include "structs.h"
+# include "prototypes.h"
+
+#endif
---
+++ Libft/includes/macros.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 10:39:10 by revieira          #+#    #+#             */
+/*   Updated: 2023/01/02 11:05:54 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+# define TRUE 1
+# define FALSE 0
+# define BUFFER_SIZE 42
+# define SPECS "cspdiuxX%"
+# define BASE_DEC "0123456789"
+# define BASE_HEX_LOW "0123456789abcdef"
+# define BASE_HEX_UPP "0123456789ABCDEF"
+
+#endif
