6a2519f064663365b511c884abf0733a3bc78994
reinanmat <reinan266@gmail.com>
2023-02-16 18:41:48 -0300

    update and refactoring

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: revieira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:14:47 by revieira          #+#    #+#             */
-/*   Updated: 2023/02/14 16:47:58 by revieira         ###   ########.fr       */
+/*   Updated: 2023/02/16 17:03:37 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ typedef struct s_stack
 {
 	int				number;
 	int				idx;
-	int				pos;
-	int				tag_pos;
+	int				curr_pos;
+	int				target_pos;
 	struct s_stack	*next;
 }					t_stack;
 
@@ -30,8 +30,8 @@ typedef struct s_data
 	int				size;
 	int				*numbers;
 	int				*simpl_numbers;
-	t_stack			*a;
-	t_stack			*b;
+	t_stack			*s_a;
+	t_stack			*s_b;
 }					t_data;
 
 //utils
@@ -53,9 +53,9 @@ int					check_order(int *int_array, int size);
 int					check_args(int argc, char **argv);
 
 //algorithm
-void				three_numbers(t_stack **header);
-void				four_numbers(t_stack **a, t_stack **b);
-void				five_numbers(t_stack **a, t_stack **b);
+void				three_numbers(t_stack **stack);
+void				four_numbers(t_stack **s_a, t_stack **s_b);
+void				five_numbers(t_stack **s_a, t_stack **s_b);
 
 void				new_algorithm(t_stack *a, t_stack *b);
 int					middle_stack(t_stack **stack);
@@ -63,22 +63,34 @@ int					middle_stack(t_stack **stack);
 //linked_stack
 t_stack				*new_node(int value, int index);
 void				add_node_back(t_stack **head_stack, t_stack *new);
-void				add_to_stack(t_stack **stack, int value, int index);
+void				add_to_stack(t_stack **head_stack, int value, int index);
 void				free_stack(t_stack **head_stack);
 int					size_stack(t_stack **stack);
 
+//pos
+void				get_pos(t_stack	*stack);
+int					middle_stack(t_stack **stack);
+
+//new_algorith,
+void				calculate_target_pos(t_stack **s_a, t_stack **s_b);
+
+//calculate_cost
+int					ft_abs(int num);
+int					get_small_cust(t_stack	**s_b);
+void				exec_small_cost(t_stack	**s_a, t_stack **s_b, int idx);
+
 //operations
-void				exec_operation(char *oper, t_stack **a, t_stack **b);
-int					push_a(t_stack **head_a, t_stack **head_b);
-int					push_b(t_stack **head_a, t_stack **head_b);
-int					swap_a(t_stack **head);
-int					swap_b(t_stack **head);
-int					swap_ab(t_stack **head_a, t_stack **head_b);
-int					rotate_a(t_stack **head);
-int					rotate_b(t_stack **head);
-int					rotate_ab(t_stack **head_a, t_stack **head_b);
-int					reverse_rotate_a(t_stack **head);
-int					reverse_rotate_b(t_stack **head);
-int					reverse_rotate_ab(t_stack **head_a, t_stack **head_b);
+void				exec_operation(char *oper, t_stack **s_a, t_stack **s_b);
+int					push_a(t_stack **s_a, t_stack **s_b);
+int					push_b(t_stack **s_a, t_stack **s_b);
+int					swap_a(t_stack **stack);
+int					swap_b(t_stack **stack);
+int					swap_ab(t_stack **s_a, t_stack **s_b);
+int					rotate_a(t_stack **stack);
+int					rotate_b(t_stack **stack);
+int					rotate_ab(t_stack **s_a, t_stack **s_b);
+int					reverse_rotate_a(t_stack **stack);
+int					reverse_rotate_b(t_stack **stack);
+int					reverse_rotate_ab(t_stack **s_a, t_stack **s_b);
 
 #endif
