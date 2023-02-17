8b0cfe873606493c64b4ce291f4275f9cd931f23
reinanmat <reinan266@gmail.com>
2023-02-17 18:59:36 -0300

    fixing double moves

--- sources/exec_operation.c
+++ sources/exec_operation.c
@@ -6,7 +6,7 @@
 /*   By: revieira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:04:38 by revieira          #+#    #+#             */
-/*   Updated: 2023/02/16 17:06:45 by revieira         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:25:17 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ int	swap_operations(char *oper, t_stack **s_a, t_stack **s_b)
 		func_ret = swap_a(s_a);
 	else if (!ft_strcmp(oper, "sb"))
 		func_ret = swap_b(s_b);
+	else if (!ft_strcmp(oper, "ss"))
+	{
+		func_ret = swap_a(s_a);
+		func_ret = swap_b(s_b);
+	}
 	return (func_ret);
 }
 
@@ -46,13 +51,19 @@ int	rotate_operations(char *oper, t_stack **s_a, t_stack **s_b)
 	else if (!ft_strcmp(oper, "rb"))
 		func_ret = rotate_b(s_b);
 	else if (!ft_strcmp(oper, "rr"))
-		func_ret = rotate_a(s_b);
+	{
+		func_ret = rotate_a(s_a);
+		func_ret = rotate_b(s_b);
+	}
 	else if (!ft_strcmp(oper, "rra"))
 		func_ret = reverse_rotate_a(s_a);
 	else if (!ft_strcmp(oper, "rrb"))
 		func_ret = reverse_rotate_b(s_b);
 	else if (!ft_strcmp(oper, "rrr"))
-		func_ret = reverse_rotate_a(s_b);
+	{
+		func_ret = reverse_rotate_a(s_a);
+		func_ret = reverse_rotate_b(s_b);
+	}
 	return(func_ret);
 }
 
