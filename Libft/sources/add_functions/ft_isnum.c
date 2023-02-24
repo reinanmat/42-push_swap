1f2f02e5ede11014a46baa180fe5bc6b2cb75eac
reinanmat <reinan266@gmail.com>
2023-02-24 12:53:24 -0300

    fix bug as '\0'

--- Libft/sources/add_functions/ft_isnum.c
+++ Libft/sources/add_functions/ft_isnum.c
@@ -6,7 +6,7 @@
 /*   By: revieira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:31:11 by revieira          #+#    #+#             */
-/*   Updated: 2023/01/20 18:33:03 by revieira         ###   ########.fr       */
+/*   Updated: 2023/02/22 13:48:49 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ int	ft_isnum(char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
+	if (!str[i])
+		return (0);
 	while (str[i])
 	{
 		if (!ft_isdigit(str[i]))
