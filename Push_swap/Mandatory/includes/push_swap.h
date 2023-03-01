0594defa2c99dc84bd4e0593d1d3c95fabf6f34b
reinanmat <reinan266@gmail.com>
2023-03-01 14:51:31 -0300

    refactored and removed unnecessary functions

--- Push_swap/Mandatory/includes/push_swap.h
+++ Push_swap/Mandatory/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: revieira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:14:47 by revieira          #+#    #+#             */
-/*   Updated: 2023/02/27 11:28:14 by revieira         ###   ########.fr       */
+/*   Updated: 2023/03/01 14:04:24 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,18 @@ typedef struct s_data
 	t_stack			*s_b;
 }					t_data;
 
+//main
+void				init_data(t_data *data, int *int_array, int size);
+void				sort(t_data *data);
+
+//check_args
+int					*check_args(int argc, char **argv);
+
 //utils
 long long			ft_atoif(char *str);
 int					*convert_to_int_array(char **matrix, int size);
 int					*normalize_numbers(int *int_array, int size);
+int					ft_issort(t_stack **stack);
 
 //stack_utils
 t_stack				*new_node(int number, int idx);
@@ -46,29 +54,16 @@ void				add_to_stack(t_stack **head_stack, int number, int idx);
 int					size_stack(t_stack **stack);
 void				free_stack(t_stack **head_stack);
 
-//delet in utils
-void				ft_print_stack(t_stack *stack);
-void				test_stack(t_stack **s_a, t_stack **s_b);
-
-//main
-void				init_data(t_data *data, int *int_array, int size);
-void				sort(t_data *data);
-
-//check_args
-int					duplicated_number(char **argv);
-int					check_order(int *int_array, int size);
-int					*check_args(int argc, char **argv);
-
 //sort
 void				sort_three_numbers(t_stack **s_a);
 void				sort_four_numbers(t_stack **s_a, t_stack **s_b);
 void				sort_five_numbers(t_stack **s_a, t_stack **s_b);
 void				put_value_in_top(t_stack **s_a, int index, int pos);
 int					find_index(t_stack **stack, int wanted);
-int					ft_issort(t_stack **stack);
 
 //sort_big
 int					find_stack(t_stack **stack, int idx);
+int					has_small_in_stack(t_stack **stack, int idx);
 void				send_to_stack_b(t_stack **s_a, t_stack **s_b);
 void				exec_rotate_or_reverse_rotate(t_stack **s_a);
 void				sort_all(t_stack **s_a, t_stack **s_b);
@@ -76,15 +71,12 @@ void				sort_all(t_stack **s_a, t_stack **s_b);
 //get_positions
 int					middle_stack(t_stack **stack);
 void				set_pos(t_stack *stack);
-int					find_high_idx(t_stack **stack);
-int					get_approximate_highest_idx(t_stack **stack, int idx);
 void				calculate_target_pos(t_stack **s_a, t_stack **s_b);
 
 //exec_small_cost
 void				exec_small_cost(t_stack **s_a, t_stack **s_b, int idx);
 
 //calculate_cost
-int					ft_abs(int num);
 void				calculate_cost(t_stack **s_a, t_stack **s_b);
 int					get_small_cost(t_stack	**s_b);
 
